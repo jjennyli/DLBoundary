f866da5f4c74f915adc62053483df30088d386b0
= <=>
2020-07-30 18:44:59 -0400

    started working on RNN (ref loaded)

---
+++ multiplication/RNN/arithmetic_datasets.py
@@ -0,0 +1,75 @@
+import random
+import numpy as np
+
+def gen_data_add(num_examples, range_start, range_end, integers_only=False):
+    X = []
+    y = []
+    for ex in range(num_examples):
+        num1 = int(random.random()*(range_end - range_start) + range_start)
+        num2 = int(random.random()*(range_end - range_start) + range_start)
+        
+        if integers_only:
+            num1 = int(num1)
+            num2 = int(num2)
+
+        X.append([num1,num2,43])        
+        y.append(num1+num2)
+    return np.array(X), np.array(y)
+
+def gen_data_sub(num_examples, range_start, range_end, integers_only=False):
+    X = []
+    y = []
+    for ex in range(num_examples):
+        num1 = int(random.random()*(range_end - range_start) + range_start)
+        num2 = int(random.random()*(range_end - range_start) + range_start)
+        
+        if integers_only:
+            num1 = int(num1)
+            num2 = int(num2)
+
+        X.append([num1,num2,145])        
+        y.append(num1-num2)
+    return np.array(X), np.array(y)
+
+def gen_data_mult(num_examples, range_start, range_end, integers_only=False):
+    X = []
+    y = []
+    for ex in range(num_examples):
+        num1 = int(random.random()*(range_end - range_start) + range_start)
+        num2 = int(random.random()*(range_end - range_start) + range_start)
+        
+        if integers_only:
+            num1 = int(num1)
+            num2 = int(num2)
+
+        X.append([num1,num2,242])        
+        y.append(num1*num2)
+    return np.array(X), np.array(y)
+
+def gen_data_div(num_examples, range_start, range_end, integers_only=False):
+    X = []
+    y = []
+    for ex in range(num_examples):
+        num1 = int(random.random()*(range_end - range_start) + range_start)
+        num2 = int(random.random()*(range_end - range_start) + range_start)
+        
+        if integers_only:
+            num1 = int(num1)
+            num2 = int(num2)
+
+        X.append([num1,num2,347])        
+        y.append(num1/num2)
+    return np.array(X), np.array(y)
+
+def gen_data(num_examples, range_start, range_end, integers_only=False):
+    num_examples = int(num_examples/4)
+    
+    ax, ay = gen_data_add(num_examples, range_start, range_end,integer_only=integer_only)
+    mx, my = gen_data_sub(num_examples, range_start, range_end,integer_only=integer_only)
+    mux, muy = gen_data_mult(num_examples, range_start, range_end,integer_only=integer_only)
+    dx, dy = gen_data_div(num_examples, range_start, range_end,integer_only=integer_only)
+    
+    X = np.concatenate((ax, mx, mux, dx))
+    y = np.concatenate((ay, my, muy, dy))
+    
+    return X, y
